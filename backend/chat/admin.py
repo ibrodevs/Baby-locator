1fda78c330beebc1468c98ffa57c0c31371b50e8
ibrodevs <ibroabdraimov20@gmail.com>
2026-08-28 12:09:04 +0600

    commit

--- backend/chat/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from .models import Message, Reward, Task
-
-admin.site.register(Message)
-admin.site.register(Task)
-admin.site.register(Reward)
--- backend/subscriptions/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from .views import RevenueCatWebhookView
-
-urlpatterns = [
-    path("webhook/", RevenueCatWebhookView.as_view()),
-]
